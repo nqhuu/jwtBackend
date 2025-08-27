3fd21cb1cb22fd78f0201d0f9afe85677fcb782a
Quoc Huu <nqhuund@gmail.com>
2025-08-27 23:11:49 +0700

    connect mysql - create user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,12 @@
+import mysql from 'mysql2';
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'jwt'
+});
+
 const handleHelloWord = (req, res) => {
     let name = "huu";
     return res.render("home.ejs", { name }) //express sẽ vào folder views để tìm file home.ejs và vì đã cấu hình trong file server nên express mới biết được khu vực lấy
@@ -7,6 +16,22 @@ const handleUser = (req, res) => {
     return res.render("user.ejs") //express sẽ vào folder views để tìm file home.ejs và vì đã cấu hình trong file server nên express mới biết được khu vực lấy
 }
 
+const handleCreateNewUser = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let username = req.body.username;
+
+    // with placeholder - insert data vào bảng user
+    connection.query(
+        'INSERT INTO users (email, password, username) VALUES(?, ?, ?); ', [email, password, username],
+        function (err, results) {
+            console.log(results);
+        }
+    );
+
+    return res.send("user-ejs") //express sẽ vào folder views để tìm file home.ejs và vì đã cấu hình trong file server nên express mới biết được khu vực lấy
+}
+
 module.exports = {
-    handleHelloWord, handleUser
+    handleHelloWord, handleUser, handleCreateNewUser
 }
--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
+import bodyParser from "body-parser";
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //config view endine
 configViewEngine(app);
